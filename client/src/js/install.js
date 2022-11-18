b504e288bf717bb71c5209d7e19f4003eb87f06f
zue <zuleikatesei@gmail.com>
2022-11-17 21:24:30 -0800

    stalewhilerevalidate in sw

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -9,13 +9,16 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
+
     const promptEvent = window.deferredPrompt;
+    console.log(window);
     if (!promptEvent) {
         return;
     }
     promptEvent.prompt();
     window.deferredPrompt = null;
     butInstall.classList.toggle('hidden', true);
+
 });
 
 // TODO: Add an handler for the `appinstalled` event
